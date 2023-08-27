92515a99b0b8c81a38bf46bd10bde3bce692a77a
abdullah-mohammed0000 <am21abdullahmohammed@gmail.com>
2023-08-27 23:15:17 +0600

    Javascript Full Course

---
+++ 9.js
@@ -0,0 +1,6 @@
+var letter = prompt("Enter any letter");
+letter = letter.toLowerCase();
+if(letter=='a'|| letter=='e'||letter=='i'||letter=='o'||letter=='u')
+console.log('Vowel')
+else
+console.log('Consonant')
