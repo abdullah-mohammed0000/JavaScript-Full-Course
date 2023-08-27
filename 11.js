d82e072c3146025e8c0767f220ffdc3cd85ac8df
abdullah-mohammed0000 <am21abdullahmohammed@gmail.com>
2023-08-28 00:59:57 +0600

    JavaScript Full Course

---
+++ 11.js
@@ -0,0 +1,16 @@
+// Vowel consotant
+
+var letter = prompt("Enter any digit");
+letter = letter.toLowerCase();
+switch(letter){
+case "a":
+case "e":
+case "i":
+case "o":
+case "u":                  
+console.log("Vowel");
+ break;
+ default:
+console.log("Consonant");
+
+} 
