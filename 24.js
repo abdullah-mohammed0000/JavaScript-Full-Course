b71d9f9f62a88ddc85e8218614b2af7ae3883b92
abdullah-mohammed0000 <am21abdullahmohammed@gmail.com>
2023-09-02 19:22:54 +0600

    JavaScript Full Course

---
+++ 24.js
@@ -0,0 +1,18 @@
+var date = new Date();
+var month = date.getMonth()
+console.log(month)
+
+var year = date.getFullYear()
+console.log(year)
+
+var time = date.getTime()
+console.log(time)
+
+var today = date.getDate()
+console.log(today)
+
+var second = date.getMilliseconds()
+console.log(second);
+
+var hours = date.getHours();
+console.log(hours);
