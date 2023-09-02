3eb91baae1621c544594bf419b3a57174964a7c8
abdullah-mohammed0000 <am21abdullahmohammed@gmail.com>
2023-09-03 05:17:02 +0600

    JavaScriopt Full Course

---
+++ 28.AND OR shorthand.js
@@ -0,0 +1,9 @@
+var fame = ' '
+var fullname = fame || 'HM Nayeem';
+console.log(fullname);
+
+var isok = true;
+isok && console.log("Eveything are alright")
+
+var isok = false;
+isok && console.log("Everything is wrong")
