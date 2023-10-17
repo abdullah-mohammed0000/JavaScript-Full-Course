6511a58aa8aaa228c41048fbca2c7575b7e125dc
abdullah-mohammed0000 <am21abdullahmohammed@gmail.com>
2023-10-17 23:07:24 +0600

    JavaScript Full Course

---
+++ 56.js
@@ -0,0 +1,19 @@
+// x=20;
+// console.log(x)
+// var x;
+
+// y=30;
+// console.log(y)
+// let y;
+
+// z=40;
+// console.log(z);
+//const z;
+
+// k=50;
+// console.log(k);
+
+"use strict"
+u=90;
+console.log(u);
+
